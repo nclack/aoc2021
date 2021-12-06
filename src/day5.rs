3551a0c3bceae552925acf15b04049698211b6e9
Nathan Clack <nclack@gmail.com>
2021-12-05 19:21:41 -0800

    day5 refactor

--- src/day5.rs
+++ src/day5.rs
@@ -43,39 +43,19 @@ impl Iterator for LinePoints {
 }
 
 impl Line {
-    fn is_horizontal(&self) -> bool {
-        let (Point((r0x, _)), Point((r1x, _))) = self.0;
-        r0x == r1x
-    }
-
-    fn is_vertical(&self) -> bool {
-        let (Point((_, r0y)), Point((_, r1y))) = self.0;
-        r0y == r1y
-    }
-
     fn is_straight(&self) -> bool {
-        self.is_horizontal() || self.is_vertical()
+        let (Point((r0x, r0y)), Point((r1x, r1y))) = self.0;
+        r0x == r1x || r0y == r1y
     }
 
     fn iter(&self) -> LinePoints {
         let (Point((r0x, r0y)), Point((r1x, r1y))) = self.0;
         let (dx, dy) = (r1x - r0x, r1y - r0y);
 
-        let step = |v| {
-            if v > 0 {
-                1
-            } else if v < 0 {
-                -1
-            } else {
-                0
-            }
-        };
-
-        let dr = (step(dx), step(dy));
-
+        // we know lines are either horizontal, vertical or diagonal
         LinePoints {
             start: (r0x, r0y),
-            dr,
+            dr: (dx.signum(), dy.signum()),
             i: 0,
             n: dx.abs().max(dy.abs()),
         }
@@ -89,8 +69,7 @@ fn parse(input: &str) -> IResult<&str, Vec<Line>> {
     fn point(input: &str) -> IResult<&str, Point> {
         map(separated_pair(number, tag(","), number), Point)(input)
     }
-    let arrow = tag(" -> ");
-    let line = map(separated_pair(point, arrow, point), Line);
+    let line = map(separated_pair(point, tag(" -> "), point), Line);
     many1(terminated(line, opt(line_ending)))(input)
 }
 
@@ -99,17 +78,14 @@ impl Part1 {
         let (rest, lines) = parse(input).unwrap();
         assert!(rest.len() == 0);
 
-        let mut hist: HashMap<(i32, i32), u16> = HashMap::new();
+        let mut hist = HashMap::new();
         for line in lines.iter().filter(|&l| l.is_straight()) {
             for p in line.iter() {
                 *hist.entry(p).or_insert(0) += 1;
             }
         }
 
-        Ok(hist
-            .iter()
-            .filter_map(|(_, v)| if *v > 1 { Some(1 as usize) } else { None })
-            .sum())
+        Ok(hist.iter().filter(|(_, &v)| v > 1).count())
     }
 }
 
@@ -118,17 +94,14 @@ impl Part2 {
         let (rest, lines) = parse(input).unwrap();
         assert!(rest.len() == 0);
 
-        let mut hist: HashMap<(i32, i32), u16> = HashMap::new();
+        let mut hist = HashMap::new();
         for line in lines.iter() {
             for p in line.iter() {
                 *hist.entry(p).or_insert(0) += 1;
             }
         }
 
-        Ok(hist
-            .iter()
-            .filter_map(|(_, v)| if *v > 1 { Some(1 as usize) } else { None })
-            .sum())
+        Ok(hist.iter().filter(|(_, &v)| v > 1).count())
     }
 }
 
