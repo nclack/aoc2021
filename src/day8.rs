62ad0721262059876f8900e2ca9ec3a7a95f457c
Nathan Clack <nclack@gmail.com>
2021-12-23 21:05:02 -0800

    start day8 part2

--- src/day8.rs
+++ src/day8.rs
@@ -1,14 +1,15 @@
 use anyhow::Result;
 use nom::{
     bytes::complete::tag,
-    character::complete::{line_ending, multispace0, one_of, space0, space1},
-    combinator::opt,
+    character::complete::{line_ending, one_of, space0,char, multispace0},
+    combinator::{opt, value},
     multi::{count, fold_many1, many1, many1_count},
-    sequence::{delimited, preceded, terminated},
-    IResult,
+    sequence::{preceded, terminated, delimited, separated_pair},
+    IResult, branch::alt, error::ParseError,
 };
 
 pub struct Part1;
+pub struct Part2;
 
 impl Part1 {
     pub fn solve(input: &str) -> Result<usize> {
@@ -41,6 +42,47 @@ impl Part1 {
     }
 }
 
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+  where
+  F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+  delimited(
+    multispace0,
+    inner,
+    multispace0
+  )
+}
+
+impl Part2 {
+    fn parse(input: &str) -> IResult<&str, Vec<(Vec<u8>,Vec<u8>)>> {
+        fn part(input: &str)->IResult<&str,u8> {
+            fold_many1(
+                alt((
+                    value(1,char('a')),
+                    value(2,char('b')),
+                    value(4,char('c')),
+                    value(8,char('d')),
+                    value(16,char('e')),
+                    value(32,char('f')),
+                    value(64,char('g')),
+                )),
+                || 0,
+                |acc,n| acc | n
+            )(input)
+        }
+        let uniques=count(ws(part),10);
+        let outputs=count(ws(part),4);
+        let line=terminated(separated_pair(uniques,tag("|"),outputs),opt(line_ending));
+        many1(line)(input)
+    }
+
+    pub fn solve(input: &str)->Result<usize> {
+        let (rest,data)=Part2::parse(input).unwrap();
+        assert_eq!(rest.len(),0);
+        todo!()
+    }
+}
+
 #[test]
 fn day8() {
     assert_eq!(
@@ -48,7 +90,7 @@ fn day8() {
         Part1::solve(include_str!("../assets/day8.0.test.txt")).unwrap()
     );
     assert_eq!(
-        26,
+        61229,
         Part2::solve(include_str!("../assets/day8.0.test.txt")).unwrap()
     );
 }
